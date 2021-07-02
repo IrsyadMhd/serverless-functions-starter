801878c39d7634ab3dca24337015a283117d7324
IrsyadMhd <mhd.irsyad@gmail.com>
2021-07-02 17:34:40 +0700

    testing to deploy

--- examples/2-basic-api/app.js
+++ examples/2-basic-api/app.js
@@ -1,1 +1,25 @@
-const result = document.querySelector('.result')
+const result = document.querySelector('.result');
+
+const fetchData = async () => {
+  const { data } = await axios.get('/api/2-basic');
+  const results = data
+    .map(result => {
+      return `
+      <article class="product">
+      <img
+        src="${result.image.url}"
+        alt="${result.name}"
+      />
+      <div class="info">
+        <h5>${result.name}</h5>
+        <h5 class="price">$${result.price}</h5>
+      </div>
+    </article>
+      `;
+    })
+    .join('');
+
+  result.innerHTML = results;
+};
+
+fetchData();
