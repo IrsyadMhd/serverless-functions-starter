962f757ee2c1c73adb52ceee1532f0170a49a63c
IrsyadMhd <mhd.irsyad@gmail.com>
2021-07-02 12:07:48 +0700

    making first function

--- functions/1-hello.js
+++ functions/1-hello.js
@@ -1,5 +1,5 @@
 // domain/.netlify/functions/1-hello
-exports.handler = async (event, content) => {
+exports.handler = async (event, context) => {
   return {
     statusCode: 200,
     body: 'Our first netlify function example',
